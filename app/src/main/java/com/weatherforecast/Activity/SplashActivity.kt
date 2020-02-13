e5c8a1a8f13d0a481a444d060311385858d9c1cf
lakshmanacentria <laxam@acentriasoftware.com>
2020-02-13 11:49:58 +0530

    added rotation animation in splash screen

--- app/src/main/java/com/weatherforecast/Activity/SplashActivity.kt
+++ app/src/main/java/com/weatherforecast/Activity/SplashActivity.kt
@@ -3,14 +3,18 @@ package com.weatherforecast.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.weatherforecast.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     private val SPLASH_TIME_OUT: Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        imageView2.startAnimation(
+            AnimationUtils.loadAnimation(this, R.anim.rotate_indefinitely) );
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
